071ff1bf39d5735fe47eee6fae0fd3efe974e244
Anna Tankielun <anna.tankielun.3@gmail.com>
2023-09-12 16:23:27 +0200

    add NavBar, Footer with styles

--- react-waiter-app/src/components/views/Footer/Footer.js
+++ react-waiter-app/src/components/views/Footer/Footer.js
@@ -1,6 +1,8 @@
 const Footer = () => {
     return (
-        <h6>Footer</h6>
+        <footer className='bg-light text-center py-3'>
+            <p className='small text-muted my-1'>Copyright © Waiter.app 2023</p>
+        </footer>
     );
 };
 
--- react-waiter-app/src/components/views/NavBar/NavBar.js
+++ react-waiter-app/src/components/views/NavBar/NavBar.js
@@ -1,6 +1,17 @@
+import { Navbar, Nav } from "react-bootstrap";
+import { Container } from 'react-bootstrap';
+import { NavLink } from "react-router-dom";
+
 const NavBar = () => {
     return (
-        <h4>NavBar</h4>
+        <Navbar bg="primary" variant="dark" expand="lg" className="mt-4 mb-4 rounded">       
+            <Container>
+            <Navbar.Brand>Waiter.app</Navbar.Brand>
+                <Nav className="ms-auto">                                           
+                    <Nav.Link as={NavLink} to="/">Home</Nav.Link>                    
+                </Nav>
+            </Container>        
+        </Navbar>
     );
 };
 
